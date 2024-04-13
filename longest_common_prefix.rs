1ff770282dc14e0860f1d824733a2419b3858ae5
SATENDRA PATEL <94542169+Satendrapatel980@users.noreply.github.com>
2024-04-13 22:19:43 +0530

    Create longest_common_prefix.rs

---
+++ longest_common_prefix.rs
@@ -0,0 +1,12 @@
+fn longest_common_prefix(strs: Vec<String>) -> String {
+    if strs.is_empty() {
+        return String::new();
+    }
+    let mut prefix = strs[0].clone();
+    for s in strs.iter().skip(1) {
+        while !s.starts_with(&prefix) {
+            prefix.pop();
+        }
+    }
+    prefix
+}
