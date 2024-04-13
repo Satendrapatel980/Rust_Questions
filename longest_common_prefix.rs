3321c9fd50410dbc40a9b119d8af1b65421d63ef
SATENDRA PATEL <94542169+Satendrapatel980@users.noreply.github.com>
2024-04-13 22:22:04 +0530

    Update longest_common_prefix.rs

--- longest_common_prefix.rs
+++ longest_common_prefix.rs
@@ -10,3 +10,13 @@ fn longest_common_prefix(strs: Vec<String>) -> String {
     }
     prefix
 }
+
+fn main() {
+    let strings = vec![
+        String::from("flower"),
+        String::from("flow"),
+        String::from("flight"),
+    ];
+    let prefix = longest_common_prefix(strings);
+    println!("Longest common prefix: {}", prefix);
+}
